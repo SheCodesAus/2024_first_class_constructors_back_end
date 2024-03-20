30819ab35cefd64bc4d9298e66757eaddd584023
roberta de cecco <roberta.dececco@gmail.com>
2024-03-20 17:49:29 +0800

    initial DB sertup only for gifts and categories models

---
+++ giftcompare/gifts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from categories.models import Category
+
+# Create your models here.
+
+class Gift(models.Model):
+    class Meta:
+        db_table = "gift"
+
+    name = models.TextField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    img =  models.URLField()
+    featured_time = models.DateTimeField()
+    source_url = models.URLField()
+    categories = models.ManyToManyField(Category, 
+                                        db_table= "gift_category",
+                                        related_name= "+",
+                                        symmetrical= False)
