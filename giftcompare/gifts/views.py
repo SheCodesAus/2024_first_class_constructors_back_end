30d51a23227fb8575d21c26f1970135c837773b3
lruizper <lruizper@gmail.com>
2024-03-21 22:02:57 +0800

    post method is working correctly, need categories populated to test further

--- giftcompare/gifts/views.py
+++ giftcompare/gifts/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Gift
 from .serializers import GiftSerializer
+from rest_framework import status
 
 
 class GiftList(APIView):
@@ -9,3 +10,9 @@ class GiftList(APIView):
         gifts = Gift.objects.all()
         serializer = GiftSerializer(gifts, many=True)
         return Response(serializer.data)
+    def post(self, request):
+        serializer=GiftSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
