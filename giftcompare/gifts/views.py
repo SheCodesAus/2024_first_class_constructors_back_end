e17eaf34cf2d2717477b072fb682f79f709ec9f5
lruizper <lruizper@gmail.com>
2024-03-22 20:24:10 +0800

    added gift detail view with 404 error handling

--- giftcompare/gifts/views.py
+++ giftcompare/gifts/views.py
@@ -16,3 +16,14 @@ class GiftList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class GiftDetail(APIView):
+    def get_object(self, pk):
+        return Gift.objects.get(pk=pk)
+    def get(self, request, pk):
+        try:
+            gift = self.get_object(pk)
+            serializer = GiftSerializer(gift)
+            return Response(serializer.data)
+        except Gift.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
