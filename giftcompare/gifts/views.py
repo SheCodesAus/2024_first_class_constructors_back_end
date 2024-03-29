62e8b96af0f03dc9830632a7d3194cac60126e92
roberta de cecco <roberta.dececco@gmail.com>
2024-03-29 17:09:26 +0800

    removed code for debug

--- giftcompare/gifts/views.py
+++ giftcompare/gifts/views.py
@@ -13,7 +13,7 @@ class GiftList(APIView):
     def get(self, request):
         gifts = Gift.objects.all()
         serializer = GiftSerializer(gifts, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data) 
     
     def post(self, request):
         serializer=GiftSerializer(data=request.data)
