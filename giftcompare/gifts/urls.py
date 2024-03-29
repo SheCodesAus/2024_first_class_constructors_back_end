613634d1a3b5cf0aa1f37ea060301721728f8f01
roberta de cecco <roberta.dececco@gmail.com>
2024-03-29 11:50:23 +0800

    featured gifts backend ready

--- giftcompare/gifts/urls.py
+++ giftcompare/gifts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
+    path('gifts/featured/', views.FeaturedGiftList.as_view() ),
     path('gifts/', views.GiftList.as_view() ),
-    path('gifts/<int:pk>/', views.GiftDetail.as_view() ),
+    path('gifts/<int:pk>/', views.GiftDetail.as_view() )   
 ]
